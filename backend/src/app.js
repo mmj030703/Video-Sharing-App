0fb205e3efa97362d9addc74cde572b2d62d3503
mmj030703 <mayankjain04467@gmail.com>
2025-01-08 15:23:39 +0530

    feat: Added cors package

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,17 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
+// Configure CORS
+const corsOptions = {
+    origin: "https://vidionix.vercel.app",
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+};
+
 // Middlewares
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cookieParser());
