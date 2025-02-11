b4262d67e0c0be0ac90fa6378095d2c6f3bab152
mmj030703 <mayankjain04467@gmail.com>
2025-02-11 12:37:53 +0530

    chore: Removed reloader function

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,23 +10,6 @@ connectDB()
         app.listen(port, () => {
             console.log("Server created successfully and listening to port", port);
         });
-
-        ////////////////////////////////////////////////////////
-        const url = `https://video-sharing-app-2n9p.onrender.com/api/v1/videos/reload`; // Replace with your Render URL
-        const interval = 30000; // Interval in milliseconds (30 seconds)
-
-        //Reloader Function
-        function reloadWebsite() {
-            fetch(url)
-                .then(response => {
-                    console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-                })
-                .catch(error => {
-                    console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-                });
-        }
-
-        setInterval(reloadWebsite, interval);
     })
     .catch(error => {
         console.log("An error occurred while creating database !", error);
--- backend/src/routes/video.route.js
+++ backend/src/routes/video.route.js
@@ -8,6 +8,5 @@ videoRouter.get("/category/:category", getAllVideosByCategory);
 videoRouter.get("/video/:id", updateVideoViews, getVideoById);
 videoRouter.get("/channel/:channelId", getVideosByChannelId);
 videoRouter.get("/search/title", getVideosByTitle);
-videoRouter.get("/reload", (req, res) => res.status(200).send("Reload Successfull !"));
 
 export default videoRouter;
